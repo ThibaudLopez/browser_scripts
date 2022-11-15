5e73e9166838e90a4c2940d3c2e69393b61ddcd5
ThibaudLopez <thibaud.lopez.schneider@mapbox.com>
2022-11-15 09:46:08 -0800

    added collab

--- GitHub/script.js
+++ GitHub/script.js
@@ -6,7 +6,7 @@ https://github.com/mapbox/secret-shield
 */
 
 var meta = document.querySelector("meta[name='octolytics-dimension-repository_public']");
-if (meta.content === 'true') {
+if (meta?.content === 'true') {
 	var style = document.createElement('style');
 	style.innerText = `
 		/* big fat red border */
@@ -23,4 +23,30 @@ if (meta.content === 'true') {
 		}
 	`;
 	document.head.appendChild(style);
+}
+
+/*
+highlight a repo if it's collab repo, to prevent accidentally posting something private
+e.g.
+https://github.com/mapbox-collab
+*/
+
+var is_collab  = document.location.pathname.split('/')[1].endsWith('-collab');
+if (is_collab) {
+	var style = document.createElement('style');
+	style.innerText = `
+		/* big fat orange border */
+		body {
+			border: 10px solid darkorange;
+		}
+		/* highlight repo label 'Public' in orange */
+		#repository-container-header .Label {
+			background: darkorange;
+		}
+		/* highlight comment textarea in orange */
+		.timeline-comment textarea {
+			background: darkorange;
+		}
+	`;
+	document.head.appendChild(style);	
 }
