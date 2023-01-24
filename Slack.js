093971ef4fe5c115e9c1dd12fbe9468c8b38dbc5
ThibaudLopez <thibaud.lopez.schneider@mapbox.com>
2023-01-23 17:10:02 -0800

    +Slack

---
+++ Slack.js
@@ -0,0 +1,10 @@
+// Slack: remove left pane to take a screenshot of right pane
+var resizer = $('div.p-resizer');
+var parent = resizer.parentElement;
+parent.removeChild(resizer.previousElementSibling);
+parent.removeChild(resizer.previousElementSibling);
+parent.removeChild(resizer.previousElementSibling);
+parent.removeChild(resizer.previousElementSibling);
+parent.removeChild(resizer);
+parent.firstElementChild;
+parent.style.gridTemplateColumns = '0 auto';
