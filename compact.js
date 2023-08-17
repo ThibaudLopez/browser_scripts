36978cb092b174b695c3fb3e7f32374576f69a76
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2023-08-17 15:30:55 -0700

    Update compact.js

--- compact.js
+++ compact.js
@@ -4,4 +4,7 @@ $x("//*").forEach(e => {
 	e.style.marginBottom = "0px";
 	e.style.paddingTop = "0px";
 	e.style.paddingBottom = "0px";
+	e.style.lineHeight = "1em";
+	e.style.fontSize = "1em";
+	// PENDING: scrollbar, remove !important
 });
