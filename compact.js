485726500f4c3eb04c1932f534052c2ed38f3072
ThibaudLopez <thibaud.lopez.schneider@gmail.com>
2024-04-29 20:20:24 -0700

    update

--- compact.js
+++ compact.js
@@ -1,4 +1,5 @@
 // compact web page for screenshots
+document.body.style.overflow = 'normal'; // un-hide scrollbars
 $x("//*").forEach(e => {
 	e.style.marginTop = "0px";
 	e.style.marginBottom = "0px";
@@ -6,5 +7,7 @@ $x("//*").forEach(e => {
 	e.style.paddingBottom = "0px";
 	e.style.lineHeight = "1em";
 	e.style.fontSize = "1em";
-	// PENDING: scrollbar, remove !important
+	e.style.justifyContent = 'normal';
+	e.style.height = 'fit-content'; // or min-content
+	// PENDING: !important
 });
